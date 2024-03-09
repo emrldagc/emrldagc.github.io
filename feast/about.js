5fedb5c760cc6a938c136e4f180ac4a000fe5402
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2024-03-09 09:53:08 -0500

    push7.2

--- feast/about.js
+++ feast/about.js
@@ -3,21 +3,6 @@ navScrollThreshold = document.querySelector("header").clientHeight - navBar.clie
 /*nav bar - end*/
 
 
-/*staff section - start*/
-const staffMembers = document.querySelectorAll(".staff_member");
-staffMembers.forEach(element => {
-    let SMImg = element.querySelector("img");
-    if(SMImg.clientHeight > SMImg.clientWidth){
-        SMImg.style.width = "100%";
-    }
-    else if(SMImg.clientWidth > SMImg.clientHeight){
-        SMImg.style.height = "100%";
-    }
-    else{}
-});
-/*staff section - end*/
-
-
 /*info section - start*/
 const sectionTxtElements = document.querySelectorAll(".info_txt");
 STETransitionIncrement = 0.25; //in seconds
