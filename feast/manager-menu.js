1f459c4dd7a6829e6390680e2cacef66789756f1
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2024-03-12 20:47:08 -0400

    push12.1

--- feast/manager-menu.js
+++ feast/manager-menu.js
@@ -8,7 +8,7 @@ class menuItem{
         this.ingredients = ingredients;
     }
 }
-let menuItems = [
+let menuItemObjs = [
     new menuItem("biscuits","appetizer","14.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
     new menuItem("something","lunch","44.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
     new menuItem("steak","lunch","74.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
@@ -30,12 +30,13 @@ const newMIIng = document.getElementById("nmi_ing");
 //create new elements from creation form
 function createNewMi(){
     let newMIObj = new menuItem(newMIName.value, newMICategory.value, newMIPrice.value, newMIImg.value, newMIDesc.value, newMIIng.value);
-    menuItems.push(newMIObj);
+    menuItemObjs.push(newMIObj);
 
     updateMIManagerList();
     updateMIClientList();
 }
-newMIBtn.addEventListener("click", () =>{createNewMi();})
+if(newMIBtn != null)
+    newMIBtn.addEventListener("click", () =>{createNewMi();})
 
 //update the list in the manager page
 function updateMIManagerList(){
@@ -48,7 +49,7 @@ function updateMIManagerList(){
             }
         });
         //create new element
-        for (let i = 0; i < menuItems.length; i++) {
+        for (let i = 0; i < menuItemObjs.length; i++) {
             //clone the blank element and remove the blank ID
             let newMIObj = blankMI.cloneNode(true);
             newMIObj.id = "";
@@ -60,12 +61,12 @@ function updateMIManagerList(){
             let newMIObjDesc = newMIObj.querySelector(".mi_desc");
             let newMIObjIng = newMIObj.querySelector(".mi_ing"); 
             //assign the correct values to the retrieved elements
-            newMIObjName.innerHTML = menuItems[i].name;
-            newMIObjPrice.innerHTML = menuItems[i].price;
-            newMIObjCtg.innerHTML = menuItems[i].category;
-            newMIObjImg.src = menuItems[i].image;
-            newMIObjDesc.innerHTML = menuItems[i].description;
-            newMIObjIng.innerHTML = menuItems[i].ingredients;
+            newMIObjName.innerHTML = menuItemObjs[i].name;
+            newMIObjPrice.innerHTML = menuItemObjs[i].price;
+            newMIObjCtg.innerHTML = menuItemObjs[i].category;
+            newMIObjImg.src = menuItemObjs[i].image;
+            newMIObjDesc.innerHTML = menuItemObjs[i].description;
+            newMIObjIng.innerHTML = menuItemObjs[i].ingredients;
             //append the new element to the list
             menuContent.append(newMIObj)
         }
