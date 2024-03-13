ed19571ec3cbccfac79bd25242f13b8b4f323270
NoneForNowButMaybeLater <86891440+NoneForNowButMaybeLater@users.noreply.github.com>
2024-03-12 20:27:31 -0400

    push12

---
+++ feast/manager-gallery.js
@@ -0,0 +1,87 @@
+class galleryItem{
+    constructor(url){
+        this.url = url;
+    }
+}
+let galleryItems = [
+    new galleryItem("images/A large spaciou 903c290d-d9fc-4518-840f-7e6921f14f80 (1).png"),
+    new galleryItem("images/events/private_dining/private_dining_1.webp"),
+    new galleryItem("images/events/wedding/wedding_1.webp"),
+    new galleryItem("images/events/gala/gala_1.webp"),
+    new galleryItem("images/events/awards/awards_1.webp"),
+    new galleryItem("images/events/wedding/wedding_1.webp"),
+    new galleryItem("images/A large spaciou 903c290d-d9fc-4518-840f-7e6921f14f80 (1).png"),
+    new galleryItem("images/events/private_dining/private_dining_1.webp"),
+    new galleryItem("images/events/wedding/wedding_1.webp"),
+    new galleryItem("images/events/gala/gala_1.webp"),
+    new galleryItem("images/events/awards/awards_1.webp"),
+    new galleryItem("images/events/wedding/wedding_1.webp")
+];
+//manager variables
+const blankGI = document.getElementById("blank_gi");
+const galleryContent = document.getElementById("gallery_content");
+const newGIBtn = document.getElementById("new_gi_btn");
+const newGIImg = document.getElementById("ngi_img");
+let GIClientLoadingDone = false;
+//client variables
+const GMSlidesClient = document.getElementById("gallery_slides");
+const gallerySection = document.getElementById("gallery");
+const blankGalleryImg = document.getElementById("black_gallery_img");
+
+//create new elements from creation form
+function createNewGi(){
+    let newGIObj = new galleryItem()
+    updateGIManagerList();
+    updateGIClientList();
+}
+if(newGIBtn != null){
+    newGIBtn.addEventListener("click", () =>{createNewGi();})
+}
+
+//update the list in the manager page
+function updateGIManagerList(){
+    if(blankGI != null){
+        //delete all preexisting elements
+        let GIEls = document.querySelectorAll(".gallery_item");
+        GIEls.forEach(element => {
+            if(element.id != "blank_gi"){
+                element.remove();
+            }
+        });
+        //create new element
+        for (let i = 0; i < galleryItems.length; i++){
+            //clone the blank element and remove the blank ID
+            let newGIObj = blankGI.cloneNode(true);
+            newGIObj.id = "";
+            //retrieve the needed elements from the cloned element
+            let newGIObjImg = newGIObj.querySelector("img");
+            newGIObjImg.src = galleryItems[i].url;
+            //append the new element to the list
+            galleryContent.append(newGIObj);
+        }
+    }
+}
+
+//update the list in the menu page
+function updateGIClientList(){   
+    for (let i = 0; i < 6; i++){
+        //update carousel
+        let clientGIEl = blankGalleryImg.cloneNode(true);
+        clientGIEl.id = "";
+        let clientGIElImg = clientGIEl.querySelector("img");
+        clientGIElImg.src = galleryItems[i].url;
+        gallerySection.append(clientGIEl);
+    }
+    for (let i = 0; i < galleryItems.length; i++){
+        //update slider
+        let clientGIObj = document.createElement("img");
+        clientGIObj.src = galleryItems[i].url;
+        GMSlidesClient.append(clientGIObj);
+    }
+    GIClientLoadingDone = true;
+}
+
+window.addEventListener("load", ()=>{
+    updateGIManagerList();
+    updateGIClientList();
+})
--- feast/manager-menu.js
+++ feast/manager-menu.js
@@ -8,7 +8,7 @@ class menuItem{
         this.ingredients = ingredients;
     }
 }
-const menuItems = [
+let menuItems = [
     new menuItem("biscuits","appetizer","14.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
     new menuItem("something","lunch","44.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
     new menuItem("steak","lunch","74.99","images/home/menu/dinner.webp","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, sapiente! Consectetur harum, ratione sed ducimus quibusdam incidunt quidem exercitationem fugiat tenetur tempora quaerat ab quam quod doloremque pariatur iure. Ullam?","Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae provident voluptatem fugiat delectus ad suscipit? Nisi laborum, distinctio blanditiis ullam quo repellendus quis, deleniti beatae, numquam molestias nemo dolor?"),
@@ -18,30 +18,62 @@ const menuItems = [
 const blankMI = document.getElementById("blank_mi");
 const menuCreator = document.getElementById("menu_creator");
 const menuContent = document.getElementById("menu_content");
-//creation form elements
+const newMIBtn = document.getElementById("new_mi_btn");
+//new mi inputs
+const newMIName = document.getElementById("nmi_name");
+const newMICategory = document.getElementById("nmi_ctg");
+const newMIPrice = document.getElementById("nmi_price");
+const newMIImg = document.getElementById("nmi_img");
+const newMIDesc = document.getElementById("nmi_desc");
+const newMIIng = document.getElementById("nmi_ing");
 
+//create new elements from creation form
+function createNewMi(){
+    let newMIObj = new menuItem(newMIName.value, newMICategory.value, newMIPrice.value, newMIImg.value, newMIDesc.value, newMIIng.value);
+    menuItems.push(newMIObj);
+
+    updateMIManagerList();
+    updateMIClientList();
+}
+newMIBtn.addEventListener("click", () =>{createNewMi();})
+
+//update the list in the manager page
 function updateMIManagerList(){
-    for (let i = 0; i < menuItems.length; i++) {
-        //clone the blank element and remove the blank ID
-        let newMIEl = blankMI.cloneNode(true);
-        newMIEl.id = "";
-        //retrieve the needed elements from the cloned element
-        let newMIElName = newMIEl.querySelector(".mi_name");
-        let newMIElPrice = newMIEl.querySelector(".mi_price");
-        let newMIElCtg = newMIEl.querySelector(".mi_category")
-        let newMIElImg = newMIEl.querySelector(".mi_img");
-        let newMIElDesc = newMIEl.querySelector(".mi_desc");
-        let newMIElIng = newMIEl.querySelector(".mi_ing"); 
-        //assign the correct values to the retrieved elements
-        newMIElName.innerHTML = menuItems[i].name;
-        newMIElPrice.innerHTML = menuItems[i].price;
-        newMIElCtg.innerHTML = menuItems[i].category;
-        newMIElImg.src = menuItems[i].image;
-        newMIElDesc.innerHTML = menuItems[i].description;
-        newMIElIng.innerHTML = menuItems[i].ingredients;
-        //append the new element to the list
-        menuContent.append(newMIEl)
+    if(blankMI != null){
+        //delete all preexisting elements
+        let MIEls = document.querySelectorAll(".menu_item");
+        MIEls.forEach(element => {
+            if(element.id != "blank_mi"){
+                element.remove();
+            }
+        });
+        //create new element
+        for (let i = 0; i < menuItems.length; i++) {
+            //clone the blank element and remove the blank ID
+            let newMIObj = blankMI.cloneNode(true);
+            newMIObj.id = "";
+            //retrieve the needed elements from the cloned element
+            let newMIObjName = newMIObj.querySelector(".mi_name");
+            let newMIObjPrice = newMIObj.querySelector(".mi_price");
+            let newMIObjCtg = newMIObj.querySelector(".mi_category")
+            let newMIObjImg = newMIObj.querySelector(".mi_img");
+            let newMIObjDesc = newMIObj.querySelector(".mi_desc");
+            let newMIObjIng = newMIObj.querySelector(".mi_ing"); 
+            //assign the correct values to the retrieved elements
+            newMIObjName.innerHTML = menuItems[i].name;
+            newMIObjPrice.innerHTML = menuItems[i].price;
+            newMIObjCtg.innerHTML = menuItems[i].category;
+            newMIObjImg.src = menuItems[i].image;
+            newMIObjDesc.innerHTML = menuItems[i].description;
+            newMIObjIng.innerHTML = menuItems[i].ingredients;
+            //append the new element to the list
+            menuContent.append(newMIObj)
+        }
     }
-    //console.log(menuContent.querySelectorAll(".menu_item"))
 }
 window.onload = updateMIManagerList();
+
+//update the list in the menu page
+function updateMIClientList(){
+
+}
